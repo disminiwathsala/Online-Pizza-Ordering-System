4ad33fdb8fa419bd47aebff8bcba689a88080826
disminiwathsala <139047477+disminiwathsala@users.noreply.github.com>
2023-12-28 12:07:53 +0530

    first committ

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">Copyright © 2023 Designed by <a href="" target="_blank" rel="noopener noreferrer">@PIZZA MANIA</a></p>
+</div>
